5fd6ca2188b2f149c75e64d44ca35b77af4d53fc
rtysonCLAS12 <r.tyson.1@research.gla.ac.uk>
2021-06-18 16:32:15 +0100

    Changed tester class

--- src/org/jlab/trigger/Tester.java
+++ src/org/jlab/trigger/Tester.java
@@ -1,13 +1,19 @@
 package org.jlab.trigger;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 import org.jlab.groot.data.GraphErrors;
 import org.jlab.groot.data.H1F;
 import org.jlab.groot.ui.TCanvas;
-
+import org.jlab.jnp.hipo4.data.Bank;
+import org.jlab.jnp.hipo4.data.Event;
+import org.jlab.jnp.hipo4.io.HipoReader;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
@@ -27,52 +33,28 @@ public class Tester {
 	 * Main Function, runs the different relevant tests.
 	 */
 	public static void main(String[] args) {
+		int NEvents=6000;
+		//Load Data, last two arguments are the minimum and maximum amount of superlayer segments
+		MultiDataSet Data=Tester.ParseDataForTesting(NEvents,5,6);
+		String networkLoc="trained_model.h5";
 		
-		/*
-		 * When we initialise the Clas12AIElTriggerHipo we need to pass the number of events to predict on.
-		 * Each event has 6 predictions, one per sector.
-		 * We can then read and predict on these events using the Predict function.
-		 * The output of the classifer (response) is the probability that an event is of the positive
-		 * sample in column 0, and of the negative sample in column 1. We can convert this to just
-		 * a list of 1 if the classifier predicts that an electron is present, and 0 otherwise by
-		 * applying a lower threshold to the response.
-		 * We can then loop over each sector in each event to call or not the trigger for that sector.
-		 */
-		int NEvents=100;
-		Clas12AIElTriggerHipo AITrigger = new Clas12AIElTriggerHipo(fName,NEvents);
+		//You can save the datafiles by uncommenting the next line.
+		//Tester.SaveData(Data, "/home/richardt/PhD1/aiTrigger/eclipse-wkspace/AITrigger/training/data/");
+		
+		//Here we measure the EventRate as a function of BatchSize.
+		Tester.predRateVsBatchSize(Data,networkLoc);
+		
+		 //plot the metrics used to evaluate the AI Trigger by specifying verbose=true.
 		int BatchSize=600;
-		
-		Instant start = Instant.now();
-		INDArray output=AITrigger.Predict(BatchSize);
-		Instant end = Instant.now();
-		double timeElapsed = Duration.between(start, end).toNanos()*1e-9; 
-		double rate=BatchSize/timeElapsed;
-		System.out.format("%nTook %.3f to read and predict on %d Events for Batch Size of %d %n",timeElapsed,NEvents,BatchSize);
-		System.out.format(" ie rate of %.3f events per second %n%n",rate);
-		
-		int[] roundedOutput=AITrigger.ApplyResponseThreshold(output,0.2);
-		
-		for(int event=0; event<(NEvents*6);event+=6) {
-			for(int sector=0;sector<6;sector++) {
-				if(roundedOutput[event+sector]==1) {
-					System.out.println("Event "+event+" is predicted to have an electron in sector "+sector);
-				}
-			}
-		}
-		System.out.println("");
-		
-		/*
-		 * Here we want to reload data for testing. This is due to the fact that for testing we need to sort
-		 * samples based on whether there was an electron or not in the sector.
-		 * Instead of reading in 6*NEvents we can just pass the number of predictions directly.
-		 * The ParseDataForTesting returns our testing dataset, which we'll then use for different tests.
-		 */
-		MultiDataSet Data=AITrigger.ParseDataForTesting(NEvents*10*6,5,6);
-		String networkLoc=AITrigger.GetNetworkLocation();
-		
-		/*
-		 * Here we measure the EventRate as a function of BatchSize.
-		 */
+		int NBatches=(NEvents)/BatchSize;
+		boolean verbose=true;
+		Tester.Test(Data, NBatches, BatchSize,1,verbose,networkLoc);
+	}
+	
+	/*
+	 * 
+	 */
+	public static void predRateVsBatchSize(MultiDataSet Data, String networkLoc){
 		int bestBatchSize=0;//Prediction Rate changes with batch size
 		double bestAvRate=0;
 		int TrialPerBatch=100;
@@ -93,14 +75,47 @@ public class Tester {
 		System.out.format("Best Event Rate %.3f for Batch Size of %d %n%n",bestAvRate,bestBatchSize);
 		PlotRatesVSBatchSize(gRates, "Prediction");
 		PlotRatesVSBatchSize(gRatesEvent, "Event");
-		
-		/*
-		 * Here we plot the metrics used to evaluate the AI Trigger by specifying verbose=true.
-		 */
-		
-		int NBatches=(NEvents*10*6)/BatchSize;
-		boolean verbose=true;
-		Test(Data, NBatches, BatchSize,1,verbose,networkLoc);
+	}
+	
+	
+	/*
+	 * Function to save your dataset to a desired location. The events will be separated into
+	 * the positive and negative samples as this makes life easier for training and testing purposes.
+	 * 
+	 * Arguments:
+	 * 			Data: ND4J MultiDataSet containing the DC and EC features and the labels
+	 * 			loc: string to the output directory
+	 */
+	public static void SaveData(MultiDataSet Data, String loc) {
+		long nPreds=Data.getFeatures()[0].shape()[0];
+		//Separate signal and bg files
+		INDArray DataArraySignal=Nd4j.zeros(nPreds/2,6,184);
+		INDArray DataArrayBg=Nd4j.zeros(nPreds/2,6,184);
+		INDArray Labels=Data.getLabels()[0];
+		int sigPred=0,bgPred=0;
+		for(long i=0; i< nPreds;i++) {
+			//Get DC and EC arrays at index 0 and 1 of Data.getFeatures() and remove channel (ie 4th dimension)
+			INDArray DC=Data.getFeatures()[0].get(NDArrayIndex.point(i), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.point(0));
+			INDArray EC=Data.getFeatures()[1].get(NDArrayIndex.point(i), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.point(0));
+			INDArray Both=Nd4j.hstack(DC,EC);//stack DC and EC arrays for output
+			//Fill signal and bg arrays
+			if(Labels.getFloat(i,0)==1) {
+				DataArraySignal.get(NDArrayIndex.point(sigPred), NDArrayIndex.all(), NDArrayIndex.all()).assign(Both);
+				sigPred++;
+			} else if(Labels.getFloat(i,0)==0) {
+				DataArrayBg.get(NDArrayIndex.point(bgPred), NDArrayIndex.all(), NDArrayIndex.all()).assign(Both);
+				bgPred++;
+			}
+		}
+		//Write files to desired location
+		File fileSignal = new File(loc+"signal.npy");
+		File fileBg = new File(loc+"bg.npy");
+		try {
+			Nd4j.writeAsNumpy(DataArraySignal,fileSignal);
+			Nd4j.writeAsNumpy(DataArrayBg,fileBg);
+		} catch (IOException e) {
+			System.out.println("Could not write file");
+		}
 	}
 	
 	/*
@@ -226,8 +241,7 @@ public class Tester {
 		//if asked for, get the CLAS12 Trigger predictions and truth
 		if(wC12TriggerComp) {
 			System.out.println("Re-reading File to Calculate C12 Trigger Purity:");
-			Clas12AIElTriggerHipo dp = new Clas12AIElTriggerHipo(fName, NEvents);
-			C12TriggerAndP= dp.GetFileTriggerAndTruth();
+			C12TriggerAndP= GetFileTriggerAndTruth(NEvents);
 			System.out.format("%n C12Trigger Purity: %.3f%n%n",calculateCLAS12TriggerPurity(NEvents, C12TriggerAndP[0]));
 		}
 		
@@ -575,5 +589,286 @@ public class Tester {
 		
 		
 	}//End of PlotResponse
+	
+	/*
+	 *  Create DC images for a given sector.
+	 *  Also places requirements on the number of superlayers with at least one hit
+	 *  
+	 * Arguments:
+	 *  		dchits: Bank containing information from the drift chambers.
+	 *  		sector: sector for which to create the image
+	 *  		minSL: minimal amount of superlayers hit (>=minSL)
+	 *  		maxSL: maximal amount of superlayers hit (<=maxSL)
+	 *  
+	 * Returns:
+	 * 			EC image for a given sector.
+	 */
+	public static INDArray FillDCArray(Bank dchits, int sector, int minSL, int maxSL) {
+		//Initialise array to all zeros
+		INDArray DCVals = Nd4j.zeros(6,112);
+		//Use following vector to check how many superlayers have at least one hit
+		Vector<Integer> SLs = new Vector<Integer>();
+		for (int k = 0; k < dchits.getRows(); k++) {
+			int sectorDC = dchits.getInt("sector", k);
+			if (sectorDC == sector) { //check that the hits are in the right sector
+				int wire = dchits.getInt("wire", k);
+				int superlayer = dchits.getInt("superlayer", k);
+				//need to increment by 1/6 not assign 1/6!!
+				double tempElement=DCVals.getDouble(superlayer-1,wire-1) + 1.0/6.0;
+				//array index 0-5 not 1-6
+				DCVals.putScalar(new int[] {superlayer-1,wire-1}, tempElement);
+				if (!SLs.contains(superlayer)) {
+					SLs.add(superlayer);
+				}
+			}
+		}
+		//check that we have enough superlayers hit
+		if(SLs.size()>=minSL && SLs.size()<=maxSL) {
+			return DCVals;
+		} else {
+			return Nd4j.zeros(6,112);
+		}
+	}//END FillDCArray
+	
+
+	/*
+	 *  Create EC images for a given sector.
+	 *  
+	 * Arguments:
+	 *  		echits: Bank containing information from the FD calorimeters.
+	 *  		sector: sector for which to create the image
+	 *  
+	 * Returns:
+	 * 			EC image for a given sector.
+	 */
+	public static INDArray FillECArray(Bank echits, int sector) {
+		//Initialise array to all zeros
+		INDArray ECVals = Nd4j.zeros(6,72);
+		
+		for (int k = 0; k < echits.getRows(); k++) {
+			float energy = echits.getFloat("energy", k)/3;
+			int strip = echits.getInt("strip", k);
+			int sectorEC = echits.getInt("sector", k);
+			int layer=echits.getInt("layer", k);
+			if(sectorEC==sector) {//check that the hits are in the right sector
+				//Layer 1-3: PCAL, 4-6: ECin, 7-9: ECout
+				//Array indexing Rows 0-2: PCAL, 3-5: ECin + ECout (strips 0-71)
+				//Array indexing columns: 0-35: ECin, 36-71: ECout
+				if(layer>6) {
+					strip=strip+36;
+					layer=layer-3;
+				} 
+				ECVals.putScalar(new int[] {layer-1,strip-1}, energy);
+				
+			}
+		}//loop over echits rows
+		return ECVals;
+	}//END FillECArray
+	
+	
+	/*
+	 * Returns the data parsed into the correct format for the AI Trigger classifier.
+	 * This is meant for testing purposes, and therefore returns a ND4J MultiDataset
+	 * that also contains the true Labels for each event and the momentum of the DC track.
+	 * 
+	 * Arguments:
+	 *			nPreds: The number of predictions desired.
+	 *			minSL: The minimum amount of superlayers with a hit
+	 *			maxSL: The maximal amount of superlayers with a hit
+	 * 
+	 * Returns:
+	 * 			ND4J MultiDataSet containing DC and EC images for both positive and negative sample events,
+	 * the Labels used to distinguish these and the momentum of the particle associated with the DC track.
+	 * 	 
+	 */
+	public static MultiDataSet ParseDataForTesting(int nPreds, int minSL, int maxSL) {
+		INDArray DCArray=Nd4j.zeros(nPreds,6,112,1);
+		INDArray ECArray=Nd4j.zeros(nPreds,6,72,1);
+		INDArray Labels=Nd4j.zeros(nPreds,2);
+		INDArray Ps=Nd4j.zeros(nPreds,1);
+		HipoReader reader = new HipoReader();
+		reader.open(fName);
+		Event event = new Event();
+		Bank dchits = new Bank(reader.getSchemaFactory().getSchema("TimeBasedTrkg::TBHits"));
+		Bank echits = new Bank(reader.getSchemaFactory().getSchema("ECAL::hits"));
+		Bank parts = new Bank(reader.getSchemaFactory().getSchema("REC::Particle"));
+		Bank track = new Bank(reader.getSchemaFactory().getSchema("REC::Track"));
+		int nPred=0, nPosPred=0, nNegPred=0;
+		while (reader.hasNext() == true && nPred<nPreds) {
+			reader.nextEvent(event);
+			event.read(dchits);
+			event.read(echits);
+			event.read(parts);
+			event.read(track);
+			
+			/* Use the following vectors to store the pindexes of electrons in the event,
+			 * and the sectors were they were reconstructed.
+			 * pByIndex and pBySector allow to store the momentum of the particle
+			 * at a given pindex or in a given sector.
+			 */
+			Vector<Integer> elIndexes = new Vector<Integer>();
+			Vector<Integer> elSectors = new Vector<Integer>();
+			Map<Integer, Double> pByIndex= new HashMap<Integer,Double>();
+			Map<Integer, Double> pBySector= new HashMap<Integer,Double>();
+			for (int i = 0; i < parts.getRows(); i++) {
+				int pid = parts.getInt("pid", i);
+				int status = parts.getInt("status", i);
+				double px=parts.getFloat("px", i);
+				double py=parts.getFloat("py", i);
+				double pz=parts.getFloat("pz", i);
+				if (Math.abs(status) >= 2000 && Math.abs(status) < 4000) {
+					pByIndex.put(i,Math.sqrt(px*px+py*py+pz*pz));
+					if (pid == 11) {
+						elIndexes.add(i);
+					}
+				}
+			}
+			/*
+			 * Unfortunately the sector of a particle isn't contained in the REC::Particle bank
+			 * so we use the track bank, which has pindex and sector information.
+			 */
+			for (int k = 0; k < track.getRows(); k++) {
+				int pindex = track.getInt("pindex", k);
+				int sectorTrk = track.getInt("sector", k);
+				if(pByIndex.get(pindex)!=null) {
+					pBySector.put(sectorTrk,pByIndex.get(pindex));
+				}
+				if (elIndexes.contains(pindex)) {
+					elSectors.add(sectorTrk);
+				}
+			}
+			
+			//loop over each sector
+			for(int sector=1;sector<7;sector++) {
+				INDArray EventDCArray=FillDCArray(dchits,sector, minSL, maxSL);
+				INDArray EventECArray=FillECArray(echits,sector);
+				
+				if(EventDCArray.any() && EventECArray.any()) { //check that the images aren't all empty
+					if(elSectors.contains(sector) && nPosPred<nPreds) { //check for reconstructed electron in sector
+						DCArray.get(NDArrayIndex.point(nPred), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.all()).assign(EventDCArray);
+						ECArray.get(NDArrayIndex.point(nPred), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.all()).assign(EventECArray);
+						Labels.putScalar(new int[] {nPred,0}, 1); //column 0 set to 1 (both cols initialised to 0)
+						if(pBySector.get(sector)!=null) {
+							Ps.putScalar(new int[] {nPred,0}, pBySector.get(sector));
+						}
+						nPosPred++;
+						nPred++;
+					} else {
+						if(nNegPred<nPosPred) {//Make sure to balance dataset
+							DCArray.get(NDArrayIndex.point(nPred), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.all()).assign(EventDCArray);
+							ECArray.get(NDArrayIndex.point(nPred), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.all()).assign(EventECArray);
+							Labels.putScalar(new int[] {nPred,1}, 1);//column 1 set to 1 (both cols initialised to 0)
+							if(pBySector.get(sector)!=null) {
+								Ps.putScalar(new int[] {nPred,0}, pBySector.get(sector));
+							}
+							nNegPred++;
+							nPred++;
+						}
+					}//Electron or not 
+				}// Only take non null arrays
+			}// Loop over sectors
+		} //Read Event
+		System.out.println("Number of Predictions "+nPred+" Positive: "+nPosPred+" Negative: "+nNegPred);
+		INDArray[] toDS=new INDArray[2];
+		toDS[0]=DCArray;
+		toDS[1]=ECArray;
+		INDArray[] toDSLabels=new INDArray[2];
+		toDSLabels[0]=Labels;
+		toDSLabels[1]=Ps;
+		MultiDataSet dataset = new MultiDataSet(toDS,toDSLabels);
+		return dataset;
+	}//End of ParseDataForTesting
+	
+	
+	/*
+	 * Returns INDArray list with the first entry being a NEvents*2 array, the first column is 1 (0) 
+	 * if the C12Trigger has been called (or not) the second column is 1 (0) if an electron is
+	 * reconstructed in the same sector (or not). The second entry is the momentum of the track
+	 *  in which the sector has been called.
+	 */
+	public static INDArray[] GetFileTriggerAndTruth(int NEvents) {
+		INDArray Trigger=Nd4j.ones(NEvents,2);
+		INDArray P=Nd4j.zeros(6*NEvents,1);
+		HipoReader reader = new HipoReader();
+		reader.open(fName);
+		Event event = new Event();
+		Bank parts = new Bank(reader.getSchemaFactory().getSchema("REC::Particle"));
+		Bank track = new Bank(reader.getSchemaFactory().getSchema("REC::Track"));
+		Bank config = new Bank(reader.getSchemaFactory().getSchema("RUN::config"));
+		int nPred=0;
+		
+		//loop over all events in file until reach limit
+		while (reader.hasNext() == true && nPred<NEvents) {
+			reader.nextEvent(event);
+			event.read(parts);
+			event.read(track);
+			event.read(config);
+
+			/* Use the following vectors to store the pindexes of electrons in the event,
+			 * and the sectors were they were reconstructed.
+			 * pByIndex and pBySector allow to store the momentum of the particle
+			 * at a given pindex or in a given sector.
+			 */
+			Vector<Integer> elIndexes = new Vector<Integer>();
+			Vector<Integer> elSectors = new Vector<Integer>();
+			Map<Integer, Double> pByIndex= new HashMap<Integer,Double>();
+			Map<Integer, Double> pBySector= new HashMap<Integer,Double>();
+			for (int i = 0; i < parts.getRows(); i++) {
+				int pid = parts.getInt("pid", i);
+				int status = parts.getInt("status", i);
+				double px=parts.getFloat("px", i);
+				double py=parts.getFloat("py", i);
+				double pz=parts.getFloat("pz", i);
+				if (Math.abs(status) >= 2000 && Math.abs(status) < 4000) {
+					pByIndex.put(i,Math.sqrt(px*px+py*py+pz*pz));
+					if (pid == 11) {
+						elIndexes.add(i);
+					}
+				}
+			}//loop over particle bank
+			
+			/*
+			 * Unfortunately the sector of a particle isn't contained in the REC::Parts bank
+			 * so we use the track bank, which has pindex and sector information.
+			 */
+			for (int k = 0; k < track.getRows(); k++) {
+				int pindex = track.getInt("pindex", k);
+				int sectorTrk = track.getInt("sector", k);
+				if(pByIndex.get(pindex)!=null) {
+					pBySector.put(sectorTrk,pByIndex.get(pindex));
+				}
+				if (elIndexes.contains(pindex)) {
+					elSectors.add(sectorTrk);
+				}
+			}//loop over track banks to get sector
+			
+			//Loop over config bank to check if the trigger was set in a sector
+			for(int k = 0; k < config.getRows(); k++) {
+				for(int sector=1; sector<7;sector++) {
+					if((config.getLong("trigger", k) & (1<<sector))!=0) { //check if trigger bit is set in sector
+						if(elSectors.contains(sector)) { // check if there's a reconstructed electron in the sector
+							if(pBySector.get(sector)!=null) {
+								P.putScalar(new int[] {nPred,0}, pBySector.get(sector));
+							}
+							nPred++;
+						} else {
+							Trigger.putScalar(new int[] {nPred,1}, 0);
+							if(pBySector.get(sector)!=null) {
+								P.putScalar(new int[] {nPred,0}, pBySector.get(sector));
+							}
+							nPred++;
+						}
+					}
+				}
+			
+			}
+			
+		}// While Reading events
+		//Creates list of INDArray outputs
+		INDArray[] output=new INDArray[2];
+		output[0]=Trigger;
+		output[1]=P;
+		return output;
+	}//End of GetFileTriggerAndTrut
 
 }
